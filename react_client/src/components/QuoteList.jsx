704a3deb05508de1d62d3606b03b7c9d90bc4769
Jay Yang <106880500+j1yang@users.noreply.github.com>
2023-11-18 09:03:50 -0500

    Client: GET most liked qutoes

--- react_client/src/components/QuoteList.jsx
+++ react_client/src/components/QuoteList.jsx
@@ -21,18 +21,26 @@ const QuoteList = ({handleQuoteClick}) => {
     }
   };
   
-  const updateFetch =() => {
-    if(searchFilterRef.current.value === 'all'){
+  const updateFetch = () => {
+    if (searchFilterRef.current.value === 'all') {
       fetchQuotes();
-    }else if(searchFilterRef.current.value === 'tag'){
+    } else if (searchFilterRef.current.value === 'tag') {
       fetchQuotesByTag();
-      
-    }else if(searchFilterRef.current.value === 'id'){
+    } else if (searchFilterRef.current.value === 'id') {
       fetchQuoteById();
-    }else{
-      console.log('not fetching now..', searchFilterRef.current.value)
+    } else if (searchFilterRef.current.value === 'mostliked') {
+      fetchMostLikedQuotes();
+    } else {
+      console.log('not fetching now...', searchFilterRef.current.value);
     }
-  }
+  };
+  const fetchMostLikedQuotes = () => {
+    axios.get('https://localhost:7082/quotes/mostliked')
+      .then(response => {
+        setQuotes(response.data);
+      })
+      .catch(error => console.error('Error fetching most liked quotes:', error));
+  };
 
   const fetchQuotes = () => {
     axios.get('https://localhost:7082/quotes')
@@ -103,12 +111,14 @@ const QuoteList = ({handleQuoteClick}) => {
       <h2 className="text-2xl font-bold mb-4">Quote List</h2>
       <div className="mb-4">
       <label htmlFor="searchFilter">Search Filter:</label>
-      <select id="searchFilter" ref={searchFilterRef} onChange={handleSearchFilterChange}>
+        <select id="searchFilter" ref={searchFilterRef} onChange={handleSearchFilterChange}>
           <option value="all">Search All</option>
           <option value="tag">Search by Tag</option>
           <option value="id">Search by ID</option>
+          <option value="mostliked">Search most liked quotes</option>
         </select>
 
+       
         {searchFilterRef.current.value === 'tag' && (
           <div>
           <label>Select Tag:</label>
@@ -134,7 +144,7 @@ const QuoteList = ({handleQuoteClick}) => {
       <ul>
         {(quotes) && quotes.map(quote => (
           <li key={quote.id} className="mb-2 hover:cursor-pointer hover:bg-gray-200	hover:text-black" onClick={() => handleQuoteClick(quote)}>
-            <div className='flex justify-between h-[30px]'>
+            <div className='flex justify-between h-[45px]'>
               <p className="text-white-800"><strong>"{quote.text}"</strong></p>
               <div className='w-[50px] h-[10px] bg-transparent border-none m-0 p-0 flex my-2' onClick={() => handleLikeClick(quote.id)}>
                 <FaThumbsUp /> 
